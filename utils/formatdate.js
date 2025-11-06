69e4bcf92e640f1176ccfc38124e5f537cba91ba
odiad293-sketch <odiad293@gmail.com>
2025-11-06 20:18:01 +0100

    Update formatdate.js

--- utils/formatdate.js
+++ utils/formatdate.js
@@ -1,1 +1,13 @@
+// utils/formatdate.js
 
+exports.formatdate = (date) => {
+  const d = new Date(date);
+  return d.toLocaleString('en-US', {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+};

e3c4e56b6e5e9b2110c598d533540b99c16d3370
odiad293-sketch <odiad293@gmail.com>
2025-11-06 08:38:58 +0100

    Update mainroutes.js

--- routes/mainroutes.js
+++ routes/mainroutes.js
@@ -1,1 +1,11 @@
+// mainroutes.js
+const express = require('express');
+const router = express.Router();
+const maincontroller = require('../controllers/maincontroller');
 
+// main pages
+router.get('/', maincontroller.gethome);
+router.get('/about', maincontroller.getabout);
+router.get('/chat', maincontroller.getchat);
+
+module.exports = router;

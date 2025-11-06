3388041a9bac98d7133de1942195225b5045d673
odiad293-sketch <odiad293@gmail.com>
2025-11-06 08:44:34 +0100

    Update apiroutes.js

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,1 +1,11 @@
+// apiroutes.js
+const express = require('express');
+const router = express.Router();
+const apicontroller = require('../controllers/apicontroller');
 
+// public api
+router.get('/messages', apicontroller.getmessages);
+router.post('/messages', apicontroller.createmessage);
+router.delete('/messages/:id', apicontroller.deletemessage);
+
+module.exports = router;

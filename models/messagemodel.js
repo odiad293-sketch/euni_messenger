f712426bbefc9a712360212d6f4ba81b307f58db
odiad293-sketch <odiad293@gmail.com>
2025-11-06 09:20:48 +0100

    Update messagemodel.js

--- models/messagemodel.js
+++ models/messagemodel.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const messageschema = new mongoose.Schema({
+  sender: { type: String, required: true },
+  receiver: { type: String },
+  message: { type: String, required: true },
+  sentat: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('message', messageschema);

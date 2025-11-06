7c7b2f793c3d9ac290bcc760b29ea6a0e59cc3d6
odiad293-sketch <odiad293@gmail.com>
2025-11-06 20:14:26 +0100

    Update encryption.js

--- utils/encryption.js
+++ utils/encryption.js
@@ -1,1 +1,12 @@
+// utils/encryption.js
 
+const bcrypt = require('bcryptjs');
+
+exports.hashpassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+exports.comparepassword = async (password, hashed) => {
+  return await bcrypt.compare(password, hashed);
+};

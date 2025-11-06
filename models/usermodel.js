9690bb3cffcc608b274f6e7740ad2bcab5cb85f2
odiad293-sketch <odiad293@gmail.com>
2025-11-06 09:18:24 +0100

    Update usermodel.js

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const userschema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  joinedat: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('user', userschema);

a0f1fc0067abfe17c08fdbd4dff70e4229642041
odiad293-sketch <odiad293@gmail.com>
2025-11-06 20:04:07 +0100

    Update errorhandler.js

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,1 +1,9 @@
+// middleware/errorhandler.js
 
+module.exports = (err, req, res, next) => {
+  console.error('Error:', err.stack);
+  res.status(500).render('error/404', {
+    title: 'Error',
+    message: 'Something went wrong. Please try again later.'
+  });
+};

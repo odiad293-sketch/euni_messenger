4186d2717ef0306f6e2772fbf86072c536379d85
odiad293-sketch <odiad293@gmail.com>
2025-11-06 20:19:38 +0100

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,15 @@
+// utils/logger.js
 
+const fs = require('fs');
+const path = require('path');
+
+const logfile = path.join(__dirname, '../logs/app.log');
+
+exports.logmessage = (message) => {
+  const time = new Date().toISOString();
+  const fullmessage = `[${time}] ${message}\n`;
+  console.log(fullmessage);
+  fs.appendFile(logfile, fullmessage, (err) => {
+    if (err) console.error('Log write error:', err);
+  });
+};

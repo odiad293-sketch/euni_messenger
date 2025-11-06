ceb7d69b6f5a447436e5d502c0a313e58fa5a6aa
odiad293-sketch <odiad293@gmail.com>
2025-11-06 08:41:37 +0100

    Update adminroutes.js

--- routes/adminroutes.js
+++ routes/adminroutes.js
@@ -1,1 +1,19 @@
+// adminroutes.js
+const express = require('express');
+const router = express.Router();
+const admincontroller = require('../controllers/admincontroller');
+const adminauthmiddleware = require('../middleware/adminauthmiddleware');
 
+// admin login
+router.get('/loginadmin', admincontroller.getloginpage);
+router.post('/login', admincontroller.loginadmin);
+
+// admin dashboard (protected)
+router.get('/dashboard', adminauthmiddleware, admincontroller.dashboard);
+
+// manage users
+router.get('/manageusers', adminauthmiddleware, admincontroller.manageusers);
+router.get('/edit/:id', adminauthmiddleware, admincontroller.edituser);
+router.get('/delete/:id', adminauthmiddleware, admincontroller.deleteuser);
+
+module.exports = router;

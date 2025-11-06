c48cb1ece6c6f579a6534c3001e7570295fe5146
odiad293-sketch <odiad293@gmail.com>
2025-11-06 20:03:28 +0100

    Update Serverauthmiddleware.js

--- middleware/Serverauthmiddleware.js
+++ middleware/Serverauthmiddleware.js
@@ -1,1 +1,12 @@
+// middleware/serverauthmiddleware.js
 
+module.exports = (req, res, next) => {
+  const serverkey = req.headers['x-server-key'];
+  const expectedkey = process.env.SERVER_AUTH_KEY;
+
+  if (!serverkey || serverkey !== expectedkey) {
+    return res.status(403).json({ error: 'Unauthorized server access' });
+  }
+
+  next();
+};

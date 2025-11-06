70e0308dbbe43ce392e7365c457a0c892497e8b1
odiad293-sketch <odiad293@gmail.com>
2025-11-06 20:02:13 +0100

    Update adminauthmiddleware.js

--- middleware/adminauthmiddleware.js
+++ middleware/adminauthmiddleware.js
@@ -1,1 +1,13 @@
+// middleware/adminauthmiddleware.js
 
+module.exports = (req, res, next) => {
+  try {
+    if (!req.session || !req.session.adminid) {
+      return res.status(401).redirect('/admin/login');
+    }
+    next();
+  } catch (err) {
+    console.error('Admin auth error:', err);
+    res.status(500).send('Internal server error');
+  }
+};

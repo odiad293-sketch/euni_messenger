eaf04255641eee66b4b5cc0108615076bd6871d7
odiad293-sketch <odiad293@gmail.com>
2025-11-06 09:02:36 +0100

    Update serverpanelcontroller.js

--- controllers/serverpanelcontroller.js
+++ controllers/serverpanelcontroller.js
@@ -1,1 +1,22 @@
+// controllers/serverpanelcontroller.js
+exports.getlogin = (req, res) => {
+  res.render('serverpanel/loginserver', { title: 'Server Login' });
+};
 
+exports.loginserver = (req, res) => {
+  const { username, password } = req.body;
+  if (username === 'serveradmin' && password === '1234') {
+    req.session.serverAuth = true;
+    res.redirect('/server/dashboard');
+  } else {
+    res.status(401).render('serverpanel/loginserver', { error: 'Access Denied' });
+  }
+};
+
+exports.dashboard = (req, res) => {
+  res.render('serverpanel/dashboard', { title: 'Server Dashboard' });
+};
+
+exports.restartserver = (req, res) => {
+  res.json({ message: 'Server restart command sent successfully.' });
+};

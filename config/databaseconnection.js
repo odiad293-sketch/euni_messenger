e9e0e5fa27c1b42bcd177b26271dc80327d1fc40
odiad293-sketch <odiad293@gmail.com>
2025-11-07 11:17:36 +0100

    Update databaseconnection.js

--- config/databaseconnection.js
+++ config/databaseconnection.js
@@ -1,17 +1,26 @@
-// config/databaseConnection.js
-const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
-    process.exit(1); // Stop the app if connection fails
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://odiad293_db_user:<db_password>@cluster0.o6qoja1.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-};
+});
 
-module.exports = connectDB;
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);

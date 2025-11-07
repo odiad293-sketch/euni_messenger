8457ec994a8103d0dd87741ea234de2e61c6a44b
odiad293-sketch <odiad293@gmail.com>
2025-11-07 14:22:34 +0100

    Update databaseconnection.js

--- config/databaseconnection.js
+++ config/databaseconnection.js
@@ -1,20 +1,17 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const connectDB = async () => {
+const connectdb = async () => {
   try {
-    // Replace <db_password> with your real password
-    const uri = "mongodb+srv://odiad293_db_user:<db_etiosaodia>@cluster0.o6qoja1.mongodb.net/?appName=Cluster0";
-
-    await mongoose.connect(uri, {
+    const conn = await mongoose.connect(process.env.mongodb_uri, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
-
-    console.log("✅ MongoDB connected successfully!");
+    console.log(`mongodb connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
-    process.exit(1); // Stop app if DB fails
+    console.error('mongodb connection failed:', error.message);
+    process.exit(1);
   }
 };
 
-export default connectDB;
+module.exports = connectdb;

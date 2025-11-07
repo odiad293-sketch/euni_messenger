0a929a14323755c7344565c5556e0df8764d406e
odiad293-sketch <odiad293@gmail.com>
2025-11-07 12:02:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const path = require("path");
 require("./config/databaseconnection");
+app.use(express.static("public"));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,11 +13,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
-// Simple route
-app.get("/", (req, res) => {
-  res.send("🚀 Euni Messenger is running successfully on Render!");
-});
 
 // Error handler (optional)
 app.use((err, req, res, next) => {

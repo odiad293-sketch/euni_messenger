2ea984e296a80d2b2bf36fc189c1c11cf58c11a3
odiad293-sketch <odiad293@gmail.com>
2025-11-08 18:23:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layout');
 const http = require('http');
 const path = require('path');
 const helmet = require('helmet');

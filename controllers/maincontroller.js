d4ce424d61c929c22fc6fb260a9373a405e60bb4
odiad293-sketch <odiad293@gmail.com>
2025-11-09 09:31:00 +0100

    Update maincontroller.js

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -1,11 +1,22 @@
+// controllers/maincontroller.js
 exports.gethome = (req, res) => {
-  res.render('main/index', { title: 'euni messenger | home' });
+  // temporary placeholders
+  const user = null;          // will be replaced with real logged-in user later
+  const contacts = [];        // list of contacts for the user
+  const chats = [];           // recent chats for the user
+
+  res.render('main/index', {
+    title: 'Euni Messenger | Home',
+    user,
+    contacts,
+    chats
+  });
 };
 
 exports.getabout = (req, res) => {
-  res.render('main/about', { title: 'about euni messenger' });
+  res.render('main/about', { title: 'About Euni Messenger' });
 };
 
 exports.getchat = (req, res) => {
-  res.render('main/chat', { title: 'chat room' });
+  res.render('main/chat', { title: 'Chat Room' });
 };
